dc356dc32951abc6f48e91ea53357ba6b48d282e
Marcelo Junior <marcelo.jsjunior99@gmail.com>
2025-05-24 14:57:15 -0300

    Delete target directory

--- target/compiled/Anime_DB/models/anime_source.sql
+++
@@ -1,3 +0,0 @@
-
-
-select * from `analyticsprojects-447201.Anime_DB.animes_info_raw`
--- target/run/Anime_DB/models/anime_source.sql
+++
@@ -1,6 +0,0 @@
-
-
-  create or replace view `analyticsprojects-447201`.`Anime_DB`.`anime_source`
-  OPTIONS()
-  as select * from `analyticsprojects-447201.Anime_DB.animes_info_raw`;
-
