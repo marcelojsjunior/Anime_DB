3d494cd8eabc19d8c22f3bac4c75cd83372e4853
marcelojsjunior <marcelo.jsjunior99@gmail.com>
2025-05-24 14:48:44 -0300

    Ajustes na extração e carregamento dos dados

---
+++ target/compiled/Anime_DB/models/anime_source.sql
@@ -0,0 +1,3 @@
+
+
+select * from `analyticsprojects-447201.Anime_DB.animes_info_raw`
---
+++ target/run/Anime_DB/models/anime_source.sql
@@ -0,0 +1,6 @@
+
+
+  create or replace view `analyticsprojects-447201`.`Anime_DB`.`anime_source`
+  OPTIONS()
+  as select * from `analyticsprojects-447201.Anime_DB.animes_info_raw`;
+
